e5cac55d1c8a601f680f977ea741ceb17c65bdcc
ColinKujala <111202947+ColinKujala@users.noreply.github.com>
2025-05-28 11:15:05 -0700

    Update DataFrameSizeInfo classes

--- dataframe_stats.py
+++ dataframe_stats.py
@@ -10,17 +10,17 @@ from pyspark import StorageLevel
 from pyspark.sql.dataframe import DataFrame
 
 
-class DataFrameSizeInfoSimple():
+class DataFrameSizeInfo():
     """
-    DataFrameSizeInfoSimple calculates and tracks the size and number
+    DataFrameSizeInfo calculates and tracks the size and number
     of partitions for a given dataframe.
 
     Input:
         df: DataFrame
     
     Usage examples:
-        df_size_info = DataFrameSizeInfoSimple(df):
-            Creates a DataFrameSizeInfoSimple object for the given dataframe.
+        df_size_info = DataFrameSizeInfo(df):
+            Creates a DataFrameSizeInfo object for the given dataframe.
         df_size_info.print_stats():
             Prints the dataframe's size in bytes, number of partitions, and
             average bytes per partition.
@@ -29,22 +29,24 @@ class DataFrameSizeInfoSimple():
             and average bytes per partition.
     """
     
+    # Optimal Spark partition size ~128MB = 128 * 2^20 = 134,217,728 bytes
     target_partition_bytes = 134217728.0
 
     def __init__(self, df: DataFrame):
-        """Initialize the DataFrameSizeInfoSimple Object"""
+        """
+        Initialize the DataFrameSizeInfo Object
+
+        Calculate and print the dataframe statistics
+        """
         self.df = df.select("*")
         self.df.persist(StorageLevel.MEMORY_ONLY)
         self.df.count()  # Force the dataframe to be persisted
+        self._is_persisted = True
 
-        self.size_in_bytes = \
-            self.df._jdf.queryExecution().optimizedPlan().stats().sizeInBytes()
-        self.num_partitions = self.df.rdd.getNumPartitions()
-        self.avg_bytes_per_partition = self.size_in_bytes / self.num_partitions
-        self.recommended_partition_count = \
-            int(self.size_in_bytes / self.target_partition_bytes) + 1
+        self._calculate_stats()
         
         self.df.unpersist()
+        self._is_persisted = False
 
         print("DataFrame Statistics Calculated\n")
 
@@ -61,20 +63,55 @@ class DataFrameSizeInfoSimple():
             f"Recommended partition count: {self.recommended_partition_count}"
         )
     
+    def _calculate_stats(self):
+        """Calculate stats"""
+        if self._is_persisted:
+            self.size_in_bytes = self.df._jdf.queryExecution() \
+                .optimizedPlan().stats().sizeInBytes()
+            self.num_partitions = self.df.rdd.getNumPartitions()
+            self.avg_bytes_per_partition = \
+                self.size_in_bytes / self.num_partitions
+            self.recommended_partition_count = \
+                int(self.size_in_bytes / self.target_partition_bytes) + 1
+    
     def recalc_stats(self):
         """Recalculate the stats for the provided dataframe"""
         self.df.persist(StorageLevel.MEMORY_ONLY)
         self.df.count()  # Force the dataframe to be persisted
+        self._is_persisted = True
 
-        self.size_in_bytes = \
-            self.df._jdf.queryExecution().optimizedPlan().stats().sizeInBytes()
-        self.num_partitions = self.df.rdd.getNumPartitions()
-        self.avg_bytes_per_partition = self.size_in_bytes / self.num_partitions
-        self.recommended_partition_count = \
-            int(self.size_in_bytes / self.target_partition_bytes) + 1
+        self._calculate_stats()
         
         self.df.unpersist()
+        self._is_persisted = False
 
         print("DataFrame Statistics Recalculated\n")
 
         self.print_stats()
+
+
+class DataFrameSizeInfoExtra(DataFrameSizeInfo):
+    """
+    DataFrameSizeInfoExtra inherits from DataFrameSizeInfo
+
+    Adds some additional details but can take > 14x as long
+    as DataFrameSizeInfo. Recommended to only use this version
+    of the class if you need the extra details around data rows
+    per partition.
+    """
+
+    def _calculate_stats(self):
+        super()._calculate_stats()
+        if self._is_persisted:
+            self._row_counts_per_partition = \
+                self.df.rdd.mapPartitions(lambda x: [len(list(x))]).collect()
+            self._row_counts_per_partition.sort()
+    
+    def print_rows_per_partition(self):
+        for row in self._row_counts_per_partition:
+            print(f"{row:,}")
+
+    def print_stats(self):
+        super().print_stats()
+        print("Rows per partition:")
+        self.print_rows_per_partition()
